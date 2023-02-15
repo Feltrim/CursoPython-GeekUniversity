81d29f1caf56725dff883e0d6ead5dd74dcfceaf
Vitor Feltrim <99485226+Feltrim@users.noreply.github.com>
2023-02-15 11:30:08 -0300

    S4 Ex 26-30

--- Exercicios/Seção 04/ex28.py
+++ Exercicios/Seção 04/ex28.py
@@ -1,4 +1,5 @@
 num1 = int(input("Insira o primeiro valor: "))
 num2 = int(input("Insira o segundo valor: "))
 num3 = int(input("Insira o terceiro valor: "))
-print(f"A soma dos quadrados é equivalente à {num1 * num1 + num2 * num2 + num3 * num3}")
+print(
+    f"A soma dos quadrados é equivalente à {num1 * num1 + num2 * num2 + num3 * num3}")
