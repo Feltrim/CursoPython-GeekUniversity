380d0dbaea61ca2431f892e6184e73c9506c3878
Vitor Feltrim <99485226+Feltrim@users.noreply.github.com>
2023-02-16 11:39:09 -0300

    S4 Ex 31-35

--- Exercicios/Seção 04/ex35.py
+++ Exercicios/Seção 04/ex35.py
@@ -1,5 +1,5 @@
 from math import sqrt
 
-c1 = int(input("Insira a medida do primeiro cateto"))
-c2 = int(input("Insira a medida do segundo cateto"))
+c1 = int(input("Insira a medida do primeiro cateto: "))
+c2 = int(input("Insira a medida do segundo cateto: "))
 print(f"A hipotenusa desse triângulo mede {sqrt((c1 * c1) + (c2 * c2))}")
