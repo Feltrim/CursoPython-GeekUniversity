a7c6408105c12ec8f45fea61cd9e6b2b7737bc32
Vitor Feltrim <99485226+Feltrim@users.noreply.github.com>
2023-03-06 21:24:15 -0300

    S6 Ex 1-5

---
+++ Exercicios/Seção 06/ex02.py
@@ -0,0 +1,14 @@
+for i in range(1, 101):
+    print(i)
+
+i = 1
+while i <= 100:
+    print(i)
+    i += 1
+
+i = 1
+while True:
+    print(i)
+    i += 1
+    if i > 100:
+        break
---
+++ Exercicios/Seção 06/ex05.py
@@ -0,0 +1,5 @@
+soma = 0
+for i in range(10):
+    valor = float(input(f"Digite o valor {i+1}: "))
+    soma += valor
+print(f"A soma dos 10 valores é: {soma}")
