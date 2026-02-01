e2b4e19cbb2c0f27951f78efed7200c1677c176e
Feltrim <vitorfeltrim13542@gmail.com>
2026-01-31 21:28:10 -0300

    feat: Seção 03 - Ex01 e Ex02
    Solução dos Exercícios 01 e 02 da Seção 03

---
+++ exercicios/secao_03/ex_01.py
@@ -0,0 +1,13 @@
+"""
+Feito por: Vitor Feltrim
+
+Curso Python - Geek University
+Seção 03 - Exercício 01
+
+Faça um programa que leia um número inteiro e imprima-o.
+"""
+
+
+ler_numero = int(input("Insira um número inteiro: "))
+
+print(f"O número digitado foi: {ler_numero}")
---
+++ exercicios/secao_03/ex_02.py
@@ -0,0 +1,16 @@
+"""
+Feito por: Vitor Feltrim
+
+Curso Python - Geek University
+Seção 03 - Exercício 02
+
+Faça um programa que peça para o usuário digitar três
+valores inteiro e imprima a soma deles.
+"""
+
+
+ler_primeiro_numero = int(input("Insira o primeiro número inteiro: "))
+ler_segundo_numero = int(input("Insira o segundo número inteiro: "))
+ler_terceiro_numero = int(input("Insira o terceiro número inteiro: "))
+
+print(f"A soma dos números é: {ler_primeiro_numero + ler_segundo_numero + ler_terceiro_numero}")
