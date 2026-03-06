1cc967018de30e5278b05b2e431fc436fa427eb3
Feltrim <vitorfeltrim13542@gmail.com>
2026-03-06 00:54:01 -0300

    feat: Seção 03 - Ex03

---
+++ exercicios/secao_03/ex_03.py
@@ -0,0 +1,24 @@
+"""
+Feito por: Vitor Feltrim
+
+Curso Python - Geek University
+Seção 03 - Exercício 03
+
+Faça um programa que recebe três valores e apresente
+a soma dos quadrados dos valores lidos.
+"""
+
+
+ler_primeiro_numero = int(input("Insira o primeiro número inteiro: "))
+ler_segundo_numero = int(input("Insira o segundo número inteiro: "))
+ler_terceiro_numero = int(input("Insira o terceiro número inteiro: "))
+
+quadrado_primeiro_numero = ler_primeiro_numero
+quadrado_segundo_numero = ler_segundo_numero
+quadrado_terceiro_numero = ler_terceiro_numero
+
+quadrado_primeiro_numero *= quadrado_primeiro_numero
+quadrado_segundo_numero *= quadrado_segundo_numero
+quadrado_terceiro_numero *= quadrado_terceiro_numero
+
+print(f"A soma dos números é: {quadrado_primeiro_numero + quadrado_segundo_numero + quadrado_terceiro_numero}")
